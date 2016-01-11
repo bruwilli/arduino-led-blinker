9f86857a6bc5474b2f4d8b945cfd92651e995005
bruwilli <bruwillimail@gmail.com>
2016-01-11 07:03:27 -0800

    Add comments for how to display a number using LED

--- LEDBlinker.h
+++ LEDBlinker.h
@@ -44,6 +44,28 @@
 //   Blink LED on three times for 500ms, off for 300ms between, then pause for 1 second.
 //   Repeat the sequence forever.  No callback because this runs forever.
 //     led.blink(500, 300, 3, 1000, 0, NULL);
+//   Use the LED to display the value of an unsigned integer
+//   For example, to display the value 25, blink 2 times, then short pause, then blink 5 times, then long pause, then repeat
+//     unsigned int numberToDisplay = 534; // Could be whatever you want
+//     unsigned int divisor = 1; // Used to extract each digit (10, 100, 1000, etc)
+//     void displayDigit() {
+//       if (divisor == 1) {
+//         divisor = 10;
+//         while (numberToDisplay / divisor != 0) {
+//           divisor *= 10;
+//         }
+//         led.offForDuration(1000, displayDigit); // long pause between 1's digit and highest digit
+//       } else {
+//         unsigned int numBlinks = (numberToDisplay % divisor) / (divisor / 10);
+//         divisor /= 10; 
+//         if (numBlinks == 0) {
+//           led.blink(800, 0, 1, 600, 1, displayDigit);  // Do a long single blink if digit is zero
+//         } else {
+//           led.blink(250, 250, numBlinks, 600, 1, displayDigit); // blink for non-zero digit
+//         }
+//       }
+//     }
+//     displayDigit();
 
 class LEDBlinker {
 private:

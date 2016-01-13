428066fcf1b2cb867ae8c6ad068fb62a9eb2f969
bruwilli <bruwillimail@gmail.com>
2016-01-12 20:58:32 -0800

    Update after running/testing/fixing

--- LEDBlinker.cpp
+++ LEDBlinker.cpp
@@ -9,39 +9,39 @@
 
 void LEDBlinker::setPin(byte const pin)
 {
-   mPin = pin; 
+   mPin = pin;
    pinMode(pin, OUTPUT);
-   mNumBlinks = 0;
+   mStartTime = 0;
 }
 
-LEDBlinker::update() {
+void LEDBlinker::update() {
   unsigned long curTime = millis();
   if (curTime - mLastRunTime < LED_BLINKER_MIN_INTERVAL) {
     return;
   }
   mLastRunTime = curTime;
-  if (!mNumBlinks || !mPin) {
+  if (!mStartTime || !mPin) {
     return;
   }
-  
+
   unsigned long elapsedTime = curTime - mStartTime;
   unsigned int blinkDuration = mBlinkOnDuration + mBlinkOffDuration;
-  unsigned int sequenceDuration = blinkDuration + mPauseDuration - mBlinkOffDuration;
-  if (mNumSequences != 0 && elapsedTime / sequenceDuration >= mNumSequences) {
-    mNumBlinks = 0;
+  unsigned int sequenceDuration = blinkDuration * mNumBlinks + mPauseDuration - mBlinkOffDuration;
+  if (!sequenceDuration ||
+     (mNumSequences != 0 && elapsedTime / sequenceDuration >= mNumSequences)) {
+    mStartTime = 0;
     if (mCallbackFunction) {
       mCallbackFunction();
     }
     return;
   }
-  
   // Figure out where we are within a sequence
   unsigned int blinkingDuration = blinkDuration * mNumBlinks;
   unsigned int timeInSequence = elapsedTime % sequenceDuration;
   if (timeInSequence < blinkingDuration && timeInSequence % blinkDuration < mBlinkOnDuration) {
-    digitalWrite(mPin, HIGH);    
+    digitalWrite(mPin, HIGH);
   } else {
-    digitalWrite(mPin, LOW);    
+    digitalWrite(mPin, LOW);
   }
 }
 
@@ -51,16 +51,13 @@ void LEDBlinker::blink(unsigned int const blinkOnDuration,
                        unsigned int const pauseDuration,
                        unsigned int const numSequences,
                        void (*callbackFunction)()) {
-  mBlinkOnDuration = blinkOnDuration ? MAX(LED_BLINKER_MIN_INTERVAL, blinkOnDuration) : 0;
-  mBlinkOffDuration = blinkOffDuration ? MAX(LED_BLINKER_MIN_INTERVAL, blinkOffDuration) : 0;
+  mBlinkOnDuration = blinkOnDuration ? max(LED_BLINKER_MIN_INTERVAL, blinkOnDuration) : 0;
+  mBlinkOffDuration = blinkOffDuration ? max(LED_BLINKER_MIN_INTERVAL, blinkOffDuration) : 0;
   mNumBlinks = numBlinks;
-  mPauseDuration = pauseDuration;
+  mPauseDuration = pauseDuration ? max(LED_BLINKER_MIN_INTERVAL, pauseDuration) : 0;;
   mNumSequences = numSequences;
   mCallbackFunction = callbackFunction;
-  mStartTime = millis();
+  mStartTime = max(millis(), 1);
   mLastRunTime = 0;
-  if !(blinkOnDuration || blinkOffDuration) {
-    mNumBlinks = 0;
-  }
-  update();             
+  update();
 }
